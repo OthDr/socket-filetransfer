8250e2259be836b0852388d9b3e3daf36f34a394
unknown <otmax.vft@gmail.com>
2022-11-01 23:37:10 +0100

    init

---
+++ src/Client.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	private static DataOutputStream dataOutputStream = null;
+	private static DataInputStream dataInputStream = null;
+
+	public static void main(String[] args) {
+
+		String HOST = "localhost";
+		int PORT = 9999;
+		Socket socket = null;
+		File file = null;
+		InputStreamReader inputStreamReader = null;
+		OutputStreamWriter outputStreamWriter = null;
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
+
+		try {
+
+			socket = new Socket(HOST, PORT);
+			inputStreamReader = new InputStreamReader(socket.getInputStream());
+			outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+			bufferedReader = new BufferedReader(inputStreamReader);
+			bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+			dataInputStream = new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+			Scanner scanner = new Scanner(System.in);
+
+			while (true) {
+				System.out.println("Type file name you wanna download: ");
+				String message = scanner.nextLine();
+
+				bufferedWriter.write(message);
+				bufferedWriter.newLine();
+				bufferedWriter.flush();
+
+				file = new File("./Downloads/" + message);
+
+				if (file.exists()) {
+					receiveFile("copy" + message);
+				} else {
+					receiveFile(message);
+
+				}
+				if (message.equalsIgnoreCase("close"))
+					break;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (socket != null)
+					socket.close();
+
+				if (inputStreamReader != null)
+					inputStreamReader.close();
+
+				if (outputStreamWriter != null)
+					outputStreamWriter.close();
+
+				if (bufferedReader != null)
+					bufferedReader.close();
+
+				if (bufferedWriter != null)
+					bufferedWriter.close();
+				if (dataInputStream != null)
+					dataInputStream.close();
+				if (dataOutputStream != null)
+					dataOutputStream.close();
+
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// receive file function is start here
+	private static void receiveFile(String fileName) throws Exception {
+		int bytes = 0;
+//		File file = new File(fileName);
+//		boolean exists = file.exists();
+//		if (exists) {
+//			System.out.println("File already exists");
+//		} else {
+			FileOutputStream fileOutputStream = new FileOutputStream("./Downloads/" + fileName);
+
+			long size = dataInputStream.readLong(); // read file size
+			byte[] buffer = new byte[4 * 1024];
+			while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+				// Here we write the file using write method
+				fileOutputStream.write(buffer, 0, bytes);
+				size -= bytes; // read upto file size
+			}
+			// Here we received file
+			System.out.println("File is Received");
+			fileOutputStream.close();
+//		}
+	}
+
+}
---
+++ src/Server.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	private static DataOutputStream dataOutputStream = null;
+	private static DataInputStream dataInputStream = null;
+
+	public static void main(String[] args) throws IOException {
+
+		int PORT = 9999;
+		Socket socket = null;
+		InputStreamReader inputStreamReader = null;
+		OutputStreamWriter outputStreamWriter = null;
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
+
+		ServerSocket serverSocket = null;
+
+		serverSocket = new ServerSocket(PORT);
+
+		try {
+			System.out.println("Server is Starting in Port 9999");
+
+			socket = serverSocket.accept();
+			System.out.println("Connected");
+
+			inputStreamReader = new InputStreamReader(socket.getInputStream());
+			outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+			bufferedReader = new BufferedReader(inputStreamReader);
+			bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+			dataInputStream = new DataInputStream(socket.getInputStream());
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+			while (true) {
+
+				String message = bufferedReader.readLine();
+
+				System.out.println("Client: " + message);
+
+				sendFile("./FilesToSend/" + message);
+
+				bufferedWriter.write(message);
+				bufferedWriter.newLine();
+				bufferedWriter.flush();
+
+				if (message.equalsIgnoreCase("close")) {
+					break;
+				}
+
+			}
+
+			socket.close();
+			inputStreamReader.close();
+			outputStreamWriter.close();
+			bufferedReader.close();
+			bufferedWriter.close();
+
+			dataInputStream.close();
+			dataOutputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// sendFile function define here
+	private static void sendFile(String path) throws Exception {
+		int bytes = 0;
+		File file = new File(path);
+		boolean exists = file.exists();
+		System.out.println(exists);
+		if (exists) {
+
+			FileInputStream fileInputStream = new FileInputStream(file);
+
+			// Here we send the File to Server
+			dataOutputStream.writeLong(file.length());
+			// Here we break file into chunks
+			byte[] buffer = new byte[4 * 1024];
+			while ((bytes = fileInputStream.read(buffer)) != -1) {
+				// Send the file to Server Socket
+				dataOutputStream.write(buffer, 0, bytes);
+				dataOutputStream.flush();
+			}
+			// close the file here
+			fileInputStream.close();
+		} else
+			System.out.println("No such a file found");
+	}
+
+}
